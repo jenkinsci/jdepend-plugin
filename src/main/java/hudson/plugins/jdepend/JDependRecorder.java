967b803b52c50d900408de10ad8535f4716af821
Nicolas De Loof <nicolas.deloof@gmail.com>
2014-10-02 16:54:04 +0200

    hudson-jdepend directory is created on temp directory … as set on master, not slave

--- src/main/java/hudson/plugins/jdepend/JDependRecorder.java
+++ src/main/java/hudson/plugins/jdepend/JDependRecorder.java
@@ -90,11 +90,11 @@ public class JDependRecorder extends Recorder
      * 
      * @author Chris Lewis
      */
-    private FilePath copyToLocalWorkspace(FilePath currentWorkspace) throws
+    private FilePath copyToLocalWorkspace(FilePath ws) throws
     	InterruptedException, IOException {
-    	FilePath newSourceLocation = (new FilePath(new File(System.getProperty("java.io.tmpdir"))).createTempDir("hudson-jdepend", ""));
+    	FilePath newSourceLocation = ws.createTempDir("hudson-jdepend", "");
     	log("Copying remote data to " + newSourceLocation.toURI());
-    	currentWorkspace.copyRecursiveTo(newSourceLocation);
+    	ws.copyRecursiveTo(newSourceLocation);
     	log("Copy complete");
 
     	return newSourceLocation;
